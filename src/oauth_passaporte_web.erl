6e0f5bb5bed65dcfdf622359fee9b28a9dfd94c8
Vitor M. A. da Cruz <vitormazzi@gmail.com>
2014-01-05 01:06:15 -0200

    Passaporte Web example working.

--- src/oauth_passaporte_web.erl
+++ src/oauth_passaporte_web.erl
@@ -11,13 +11,15 @@
 %%   ...
 %%   2> {ok, Client} = oauth_passaporte_web:start(Consumer).
 %%   ...
-%%   3> {ok, Token} = oauth_passaporte_web:get_request_token(Client).
+%%   3> CallbackUrl = "http://sandbox.app.passaporteweb.com.br/sso/oob/".
 %%   ...
-%%   4> AuthorizeURL = oauth_passaporte_web:authorize_url(Token).
+%%   4> {ok, Token} = oauth_passaporte_web:get_request_token(Client, CallbackUrl, sandbox).
 %%   ...
-%%   5> ok = oauth_passaporte_web:get_access_token(Client, "...VERIFIER (PIN)...").
+%%   5> AuthorizeURL = oauth_passaporte_web:authorize_url(Token, sandbox).
 %%   ...
-%%   6> {ok, Headers, JSON} = oauth_passaporte_web:get_favorites(Client).
+%%   6> ok = oauth_passaporte_web:get_access_token(Client, "...VERIFIER (PIN)...", sandbox).
+%%   ...
+%%   7> {ok, Headers, JSON} = oauth_passaporte_web:fetch_user_data(Client, sandbox).
 %%   ...
 %%
 %% Note that before fetching the access token (step 5) you need to have
@@ -25,27 +27,40 @@
 %%
 -module(oauth_passaporte_web).
 
--compile(export_all).
+-export([start/1, get_request_token/3, authorize_url/2,
+    get_access_token/3, fetch_user_data/2, verify_credentials/2 ]).
 
 start(Consumer) ->
   oauth_client:start(Consumer).
 
-get_request_token(Client, CallbackUrl) ->
-  BaseUrl = "http://sandbox.app.passaporteweb.com.br/sso/initiate/?oauth_callback=",
-  URL = lists:append(BaseUrl, CallbackUrl),
-  oauth_client:get_request_token(Client, URL).
+get_request_token(Client, CallbackUrl, Environment) ->
+  URL = get_env_url("/sso/initiate/", Environment),
+  Params = [{"oauth_callback", CallbackUrl}],
+  oauth_client:get_request_token(Client, URL, Params).
 
-authorize_url(Token) ->
-  oauth:uri("http://sandbox.app.passaporteweb.com.br/sso/authorize/", [{"oauth_token", Token}]).
+authorize_url(Token, Environment) ->
+  URL = get_env_url("/sso/authorize/", Environment),
+  oauth:uri(URL, [{"oauth_token", Token}]).
 
-get_access_token(Client, Verifier) ->
-  URL = "http://sandbox.app.passaporteweb.com.br/sso/token/",
+get_access_token(Client, Verifier, Environment) ->
+  URL = get_env_url("/sso/token/", Environment),
   oauth_client:get_access_token(Client, URL, [{"oauth_verifier", Verifier}]).
 
-get_favorites(Client) ->
-  URL = "http://sandbox.app.passaporteweb.com.br/sso/fetchuserdata/",
+fetch_user_data(Client, Environment) ->
+  URL = get_env_url("/sso/fetchuserdata/", Environment),
   oauth_client:get(Client, URL, []).
 
-verify_credentials(Client) ->
-  URL = "http://sandbox.app.passaporteweb.com.br/oauth/resource_owner/",
+verify_credentials(Client, Environment) ->
+  URL = get_env_url("/oauth/resource_owner/", Environment),
   oauth_client:get(Client, URL, []).
+
+%%
+%% Helper functions
+%%
+get_env_url(Path, Environment) ->
+  Host = case Environment of
+    production -> "https://app.passaporteweb.com.br";
+    sandbox -> "http://sandbox.app.passaporteweb.com.br";
+    _ -> error
+  end,
+  Host ++ Path.
